2a0bde8a80e83632bb9e19e443e98569129568cb
elfcoda <nanohnu@gmail.com>
2021-07-03 00:30:11 +0800

    update jit

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -389,7 +389,7 @@ class AST
                 return parseTree2LLVMAST(pRoot->getChild(0));
             } else if ("NotExp" == symbolStr) {
                 std::unique_ptr<ASTBase> astNode = parseTree2LLVMAST(pRoot->getChild(1));
-                std::unique_ptr<BoolExprAST> boolNode = llvm::unique_dyn_cast_or_null<BoolExprAST, ASTBase>(std::move(astNode));
+                std::unique_ptr<BoolExprAST> boolNode = dynamic_cast_ast<BoolExprAST>(std::move(astNode));
                 boolNode->inverse();
                 return boolNode;
             } else if ("VoidExp" == symbolStr) {
